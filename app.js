e1aa989643090cb3a2343254217e4b87bdf396f9
Frank Jimenez <frankjimenezp113@gmail.com>
2023-04-11 20:15:36 -0400

    creacion y moddificacion del Index.html e Index.css

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ const __dirname = path.dirname(__filename);
 const app = express()
 const port = 8080
 
+app.use(express.static('public'));
 
+app.get('/index', (req, res) => {
+    res.sendFile( __dirname + '/public/index.html')
+})
+
+app.get('*', (req, res) => {
+    res.send("404 | Page not found")
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
